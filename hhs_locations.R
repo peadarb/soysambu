257cbb1d074d54237febcad66841e3101a081a9c
W.Esther <estherwairimuk@gmail.com>
2023-12-01 15:46:34 +0300

    Merged additional location areas

--- hhs_locations.R
+++ hhs_locations.R
@@ -5,6 +5,7 @@ library(dplyr)
 library(readxl)
 
 hhs_locations <- read_xlsx("soysambu_data_27112023.xlsx") %>% 
+hhs_locations <- read_xlsx("soysambu_data_30112023.xlsx") %>% 
   select("_97. Location (GPS)_latitude", "_97. Location (GPS)_longitude", "1. Name of Enumerator", "17. What is the household heads' village name?") %>% 
   rename("y" = "_97. Location (GPS)_latitude", "x" = "_97. Location (GPS)_longitude", "name" = "1. Name of Enumerator", "village" = "17. What is the household heads' village name?") %>%
   st_as_sf(coords = c("x", "y"), crs = 4326)
